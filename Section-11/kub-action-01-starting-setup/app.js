55ea4f5ae6f3c8b0a8fa16832af439e68fbcb978
Chipzstar <chipzstar.dev@googlemail.com>
2023-09-04 16:12:37 +0100

    add section 11

---
+++ Section-11/kub-action-01-starting-setup/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!</h1>
+    <h2>This is some new text</h2>
+    <p>Try sending a request to /error and see what happens</p>
+  `);
+});
+
+app.get('/error', (req, res) => {
+  process.exit(1);
+});
+
+console.log("App listening on port 8080...")
+app.listen(8080);
